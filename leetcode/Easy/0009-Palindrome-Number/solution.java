c4279c09fa7c733f3f50cc8acbebe67a9bf393e0
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 13:53:25 +0530

    Add LeetCode Easy - Palindrome Number

---
+++ leetcode/Easy/0009-Palindrome-Number/solution.java
@@ -0,0 +1,19 @@
+// LeetCode Problem: Palindrome Number
+// Link: https://leetcode.com/problems/palindrome-number/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        while(temp>0){
+            int rem = temp%10;
+            rev = rev*10 + rem;
+            temp = temp/10;
+        }
+        if(rev == x){
+            return true;
+        }else return false;
+    }
+}
