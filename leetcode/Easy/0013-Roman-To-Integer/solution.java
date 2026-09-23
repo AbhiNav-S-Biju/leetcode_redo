392018fb1bff9eb6e578a0608d4c12b3a54ad6c5
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-23 14:55:47 +0530

    Add LeetCode Easy - Roman to Integer

---
+++ leetcode/Easy/0013-Roman-To-Integer/solution.java
@@ -0,0 +1,66 @@
+// LeetCode Problem: Roman to Integer
+// Link: https://leetcode.com/problems/roman-to-integer/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int romanToInt(String str) {
+        int sum = 0;
+        char[] s = str.toCharArray();
+        for(int i = 0;i<s.length; i++){
+            if(s[i] == ('M')){
+                sum += 1000;
+            }
+            else if(s[i]==('D')){
+                sum += 500;
+            }
+            else if(s[i]==('C')){
+                if((i+1) < s.length && s[i+1]==('D')){
+                    sum+=400;
+                    i++;
+                }
+                else if((i+1) < s.length && s[i+1]==('M')){
+                    sum+=900;
+                    i++;
+                }
+                else{
+                    sum+=100;
+                }
+            }
+            else if(s[i]==('L')){
+                sum+= 50;
+            }
+            else if(s[i]==('X')){
+                if((i+1)<s.length && s[i+1]==('L')){
+                    sum+=40;
+                    i++;
+                }
+                else if((i+1)<s.length &&s[i+1]==('C')){
+                    sum+=90;
+                    i++;
+                }
+                else{
+                    sum+=10;
+                }
+            }
+            else if(s[i]==('V')){
+                sum+=5;
+            }
+            else{
+                if((i+1) < s.length && s[i+1]==('V')){
+                    sum+=4;
+                    i++;
+                }
+                else if((i+1) < s.length && s[i+1]==('X')){
+                    sum+=9;
+                    i++;
+                }
+                else{
+                    sum+=1;
+                }
+            }
+        }
+        return sum;
+    }
+
+}
