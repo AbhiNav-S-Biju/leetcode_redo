3e14091ff6a028dc88995e5839c0c795cf3c8f47
AbhiNav-S-Biju <abhinavbijusn@gmail.com>
2026-09-25 22:13:57 +0530

    Add LeetCode Easy - Convert the Temperature

---
+++ leetcode/Easy/2556-Convert-The-Temperature/solution.java
@@ -0,0 +1,13 @@
+// LeetCode Problem: Convert the Temperature
+// Link: https://leetcode.com/problems/convert-the-temperature/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] arr = new double[2];
+        arr[0] = celsius + 273.15;
+        arr[1] = celsius *1.80 +32.00;
+        return arr;
+    }
+}
